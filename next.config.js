47e92f72c4ef368afbfc21cba7842ea54eac76da
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-10-29 03:52:50 +0000

    add vanilla-extract

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const {
+  createVanillaExtractPlugin
+} = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,4 +13,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withVanillaExtract(nextConfig);
