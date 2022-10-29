15ec71f0ddbf1345ab9ff10c90083d0eb84bbedf
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-10-29 03:33:17 +0000

    init

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { FC,PropsWithChildren } from "react";
+
+const RootLayout:FC<PropsWithChildren> = ({children}) => {
+    return(
+        <html lang="ja">
+            <head>
+                <title>{process.env.UserName} Blog</title>
+            </head>
+            <body>
+                <p>root layouts</p>
+                <p>created by {process.env.UserName} </p>
+                {children}
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function RootIndex(){
+    return (
+        <>
+            <p>Hello!</p>
+        </>
+    )  
+}
