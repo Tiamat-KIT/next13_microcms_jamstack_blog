d42580c616784e573ddd8ac5f99c1fe825fd3160
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-10-31 05:20:31 +0000

    space add

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const RootLayout:FC<PropsWithChildren> = ({children}) => {
             <body>
             <header className="border-b border-gray-300 py-2">
                 <div className="flex items-center justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
-                <h3>{process.env.UserName}Site</h3>
+                <h3>{process.env.UserName} Site</h3>
                     <FiMenu className="lg:hidden block h-6 w-6 cursor-pointer" onClick={() => !open}/>
                     <nav className={`${open ? "block" :  "hidden" } w-full lg:flex lg:items-center lg:w-auto`}>
                         <ul className="text-base text-gray-600 lg:flex lg:justify-between">
