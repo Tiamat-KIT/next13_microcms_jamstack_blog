cfe37dc82a93514e772b86083167b1c9e7829708
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-11-02 10:05:02 +0000

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ const RootLayout:FC<PropsWithChildren> = ({children}) => {
                     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Mytemplate
                         <a  className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">RoboCup@Home Project in KIT 広報班</a>
                     </p>
-                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    {/*<span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="text-gray-500">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -72,7 +72,7 @@ const RootLayout:FC<PropsWithChildren> = ({children}) => {
                                 <circle cx="4" cy="4" r="2" stroke="none"></circle>
                             </svg>
                         </a>
-                    </span>
+                    </span>*/}
                 </div>
             </footer>   
             </body>
